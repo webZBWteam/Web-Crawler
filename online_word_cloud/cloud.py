3a1ef814ffd399f1559b24e80ccf6d6c337ba768
zbw <601767164@qq.com>
2020-07-01 20:10:53 +0800

    实现实时词云，背景图片和字体随机

--- online_word_cloud/cloud.py
+++ online_word_cloud/cloud.py
@@ -8,21 +8,9 @@ import glob
 import random
 plt.rc('figure', figsize=(15, 15))
 class data:
-    def __init__(self):
-        self.name=glob.glob('/Users/zhubowen/Desktop/Web-Crawler/online_word_cloud/*3.txt')[0]
+    def __init__(self,sear):
+        self.name='/Users/zhubowen/Desktop/Web-Crawler/online_word_cloud/'+sear+'.txt'
         self.all_text=''
-
-    #读取微博正文内容
-    def read_weibo(self):
-        file =open(self.name,encoding='utf-8')
-        i=0
-        while 1:
-            context = file.readline()
-            self.all_text = self.all_text + context
-            if len(context) ==0:
-                break
-        file.close()
-        print('succeed0')
 
     #读取微博评论内容，避开ID
     def read_pinglun(self):
@@ -39,50 +27,8 @@ class data:
         file.close()
         print('succeed0')
 
-    #读取微博+评论文档，避开无关内容
-    def read_all(self):
-        file =open(self.name,encoding='utf-8',errors='ignore')
-        i=0
-        context=file.readline()
-        while 1:
-            if '-----微博正文-----' in context:
-                i=1
-                context=file.readline()
-                self.all_text=self.all_text+context
-                while 1:
-                    context=file.readline()
-                    if '-----微博正文-----' in context:
-                        break
-                    if len(context) ==0:
-                        break
-                    i=i+1
-                    if(i==4):
-                        self.all_text = self.all_text + context
-                        i=0
-            if len(context) ==0:
-                break
-        file.close()
-        print('succeed0')
-        
-    #对每条内容进行情感分析
-    def mood(self):
-        self.all_text=self.all_text.replace(' ','')
-        sentimentslist=[]
-        for i in self.all_text:
-            s=SnowNLP(i)
-            sentimentslist.append(s.sentiments)
-        plt.hist(sentimentslist,bins=np.arange(0,1,0.01),facecolor='b')
-        plt.xlabel('情绪指数')
-        plt.ylabel('分词数量')
-        plt.title('情感分析图')
-        plt.rcParams['font.sans-serif']=['SimHei']
-        #di_zhi='C:/Users/haome/Desktop/jieguo/情感'+self.dl_name+'.jpg'
-        #plt.savefig(di_zhi)
-        plt.show()
-
     #词云的生成与数据的处理
     def ciyun(self):
-        
         #去除没有意义的虚词，虚词在unless文件中保存
         excludes = open(file='/Users/zhubowen/Desktop/Web-Crawler/词云+情感分析封装/unless.txt',encoding = 'utf-8').read().splitlines()
         for i in excludes:
@@ -101,7 +47,7 @@ class data:
         #利用worldcloud制造词云
         fonts=glob.glob('/Users/zhubowen/Desktop/Web-Crawler/online_word_cloud/Fonts/*.ttf')
         font=random.choice(fonts)
-        color_masks=glob.glob('/Users/zhubowen/Desktop/Web-Crawler/online_word_cloud/pict/*.')
+        color_masks=glob.glob('/Users/zhubowen/Desktop/Web-Crawler/online_word_cloud/pict/*.jpg')
         color_mask = random.choice(color_masks)
         cloud = wc.WordCloud( font_path=font,#设置字体  
                               background_color="white", #背景颜色  
